5520e3e6e0a1046009c5726f95cba58f67470348
Kyle Stevens <kstevens715@gmail.com>
2014-07-23 21:16:12 -0400

    Add a couple more tests

--- test/let_bang_test.rb
+++ test/let_bang_test.rb
@@ -4,11 +4,12 @@ require_relative '../lib/let_bang'
 describe Minitest::Spec, :let! do
 
   i_suck_and_my_tests_are_order_dependent!
+
   def _count
     $let_count ||= 0
   end
 
-  let! :count do
+  let!(:count) do
     $let_count ||= 0
     $let_count += 1
     $let_count
@@ -27,4 +28,20 @@ describe Minitest::Spec, :let! do
     _count.must_equal 2
   end
 
+  let!(:dependent) { dependency + 1 }
+  let!(:dependency) { 2 }
+
+  it "resolves dependencies regardless of order" do
+    dependent.must_equal 3
+  end
+
+  describe 'multiple definitions' do
+    let!(:last) { $last ||= 0; $last += 1 }
+    let!(:last) { $last ||= 0; $last += 4 }
+
+    it "only uses last definition" do
+      $last.must_equal 4
+      last.must_equal 4
+    end
+  end
 end
